e8b38a1323a0084705609c145ee54b8b1cf35555
Jeanie Lim <jeanielmj@gmail.com>
2017-01-20 10:29:48 -0500

    refactoring

--- app/src/main/java/com/example/demouser/caloriescounter/MainActivity.java
+++ app/src/main/java/com/example/demouser/caloriescounter/MainActivity.java
@@ -7,9 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
